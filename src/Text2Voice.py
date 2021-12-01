5837cc995948ff28a31b8dca6c331d2231f55495
Tara <tara20070827@gmail.com>
2021-12-01 20:17:21 +0900

    fix bug when USER_DICTIONARY has empty list.

--- src/Text2Voice.py
+++ src/Text2Voice.py
@@ -18,32 +18,6 @@ def replace_text(matching_pattern, replace_characters, text):
     return text
 
 
-def replace_text_all(text, user_dictionary):
-    # Replace unnecessary characters in text such as URL, email, user, etc.
-    # WARNING: DO NOT set no character in the replacement characters.
-    # 現状、置き換え文字を空白にしてしまうと、入力した文字によっては置き換え後の読み上げテキスト文字列が何もなくなってしまい、エラーを起こします。
-    DEFAULT_MATCHING_PATTERN = [
-        ['URL', 'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+', "URL"],
-        ['MAIL', '[\w\-._]+@[\w\-._]+\.[A-Za-z]+', 'メール'],
-        ['USER', '<@![\d]+>', 'ユーザー'],
-        ['CUSTOM_EMOJI', '<:[\w]+:[\d]+>', '絵文字'],
-        ['EMOJI', ':[\w]+:', '絵文字'],
-    ]
-
-    for replacement_set in DEFAULT_MATCHING_PATTERN:
-        pattern = replacement_set[1]
-        replace_characters = replacement_set[2]
-        text = replace_text(pattern, replace_characters, text)
-
-    # Replace user-defined characters in text
-    for replacement_set in user_dictionary:
-        pattern = replacement_set[0]
-        replace_characters = replacement_set[1]
-        text = replace_text(pattern, replace_characters, text)
-
-    return text
-
-
 class T2V(object):
 
     def __init__(self, api_key, api_endpoint, user_dictionary, speaker='show'):
@@ -78,13 +52,38 @@ class T2V(object):
 
         return self
 
+    def replace_text_all(self, text):
+        # Replace unnecessary characters in text such as URL, email, user, etc.
+        # WARNING: DO NOT set no character in the replacement characters.
+        # 現状、置き換え文字を空白にしてしまうと、入力した文字によっては置き換え後の読み上げテキスト文字列が何もなくなってしまい、エラーを起こします。
+        DEFAULT_MATCHING_PATTERN = [
+            ['URL', 'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+', "URL"],
+            ['MAIL', '[\w\-._]+@[\w\-._]+\.[A-Za-z]+', 'メール'],
+            ['USER', '<@![\d]+>', 'ユーザー'],
+            ['CUSTOM_EMOJI', '<:[\w]+:[\d]+>', '絵文字'],
+            ['EMOJI', ':[\w]+:', '絵文字'],
+        ]
+
+        for replacement_set in DEFAULT_MATCHING_PATTERN:
+            pattern = replacement_set[1]
+            replace_characters = replacement_set[2]
+            text = replace_text(pattern, replace_characters, text)
+
+        # Replace user-defined characters in text
+        for replacement_set in self.user_dictionary:
+            pattern = replacement_set[0]
+            replace_characters = replacement_set[1]
+            text = replace_text(pattern, replace_characters, text)
+
+        return text
+
     def generate_voice(self, text):
         output_dir = os.path.dirname(__file__) + '/cache'
         os.makedirs(output_dir, exist_ok=True)
         output_filename = "tmp.mp3"
         output = os.path.join(output_dir, output_filename)
 
-        read_text = replace_text_all(text, user_dictionary)
+        read_text = self.replace_text_all(text)
         self.api_preference['text'] = read_text
         result = requests.post(self.api_endpoint, data=self.api_preference, auth=(self.api_key, ""))
         if result.status_code != requests.codes.ok:
@@ -105,9 +104,9 @@ if __name__ == '__main__':
 
     voiceText_api_key = config_ini['VoiceText Web API']['API_KEY']
     voiceText_api_endpoint = config_ini['VoiceText Web API']['API_ENDPOINT']
-    user_dictionary = json.loads(config_ini['General']['USER_DICTIONARY'])
+    my_dict = json.loads(config_ini['General']['USER_DICTIONARY'])
 
-    t2v = T2V(api_key=voiceText_api_key, api_endpoint=voiceText_api_endpoint, user_dictionary=user_dictionary)
+    t2v = T2V(api_key=voiceText_api_key, api_endpoint=voiceText_api_endpoint, user_dictionary=my_dict)
     t2v.generate_voice(
         "これはテスト用に作成した音声です。https://www.google.com, hogehoge@ezweb.ne.jp, <@!123456789>, :fire:, <:fire:123456789>")
     print("OK")
